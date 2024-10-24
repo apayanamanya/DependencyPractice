41bb6559ee684ad1301a3b8a759c81f54c119424
apayanamanya <meutiarahmadhani225@gmail.com>
2024-10-24 21:07:33 +0700

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "Hello, Maven!";
+
+        System.out.println(text);
+    }
+}
